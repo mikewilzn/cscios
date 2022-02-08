edcac5806f34818c82a2f948eb799fa6143ca0cd
Michael J Wilson <mjwilson@csciarch02.muad.local>
2022-02-08 18:28:03 -0500

    Added comment about parent processes

--- preludeshell1/Shell.c
+++ preludeshell1/Shell.c
@@ -9,19 +9,35 @@
 
 int main ()
 {
-	const int CHAR_LIMIT = 80;
+	const int BUFFER_SIZE = 80;
 
-	const char *buff[] = malloc(CHAR_LIMIT * sizeof(char));
+	char buff[BUFFER_SIZE];
 
-	printf ("cmd>");
+	printf ("shell> ");
 
-	__pid_t cpid = fork();
-	__pid_t ppid = getpid()
+	fgets(buff, sizeof(buff), stdin);
+	buff[strcspn(buff, "\n")] = 0;
 
+	__pid_t pid = fork();
 
-	char *fgets(buff, CHAR_LIMIT, stdin);
-	printf(buff);
+  	if (pid < 0)
+  	{
+    	fprintf (stderr, "fork error (%s) -- exiting\n",
+	    	strerror (errno));
+    	exit (1);
+  	}
+	if (pid == 0)
+  	{
+		/* Since this is the child process, the ppid is going to be the same as the pid of the parent */
+    	printf("Child says:  PID = %d, parent PID = %d, '%s'\n", getpid(), getppid(), buff);
+    	exit (0); 
+ 	}
+	/* 
+	 * Since this is the parent process, the ppid will be the same as the pid of the process
+	 * that called this whole program. In this case that is the currently running instance of
+	 * bash that was used to execute this program.
+	 */
+	printf("Parent says: PID = %d, child PID = %d, parent PID = %d\n", getpid(), pid, getppid());
 
-	printf("Parent says: PID = %d, child PID = %d, parent PID = %d\n");
-	printf("Child says:  PID = %d, parent PID = %d, '%s'\n");
+	return 0;
 }
