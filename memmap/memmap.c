21cfdb1ef433739b6aee71fb854103721356b495
Mike Wilson <mjwilson@millersville.edu>
2022-03-28 22:21:46 -0400

    Cleaned up and documented code

--- memmap/memmap.c
+++ memmap/memmap.c
@@ -1,3 +1,11 @@
+/*
+ * Filename	: memmap.c
+ * Author	: Michael Wilson
+ * Course	: CSCI 380-01
+ * Assignment	: Memory Mapping
+ * Description	: Implement cp via the mmap function
+ */
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -17,6 +25,7 @@ main (int argc, char* argv[])
         exit(EXIT_SUCCESS);
     }
 
+    /* Open source file read-only */
     int srcFile = open(argv[1], O_RDONLY);
     if (srcFile < 0)
     {
@@ -24,6 +33,7 @@ main (int argc, char* argv[])
         exit(EXIT_FAILURE);
     }
 
+    /* Open destination file. Creating new file in read/write mode with correct permissions. */
     int destFile = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0600);
     if(destFile < 0)
     {
@@ -31,20 +41,43 @@ main (int argc, char* argv[])
         exit(EXIT_FAILURE);
     }
 
+    /* Retrieves stats on source file. Looking for size of source file. */
     struct stat info;
     fstat(srcFile, &info);
 
+    /* Sets size of destination file to that of the source file */
     if ((ftruncate(destFile, info.st_size)) < 0)
     {
         perror("ftruncate() error");
         exit(EXIT_FAILURE);
     }
 
+    /*
+     * Maps address space of source file.
+     * PROT_READ so mapping can be read.
+     * MAP_SHARED so mapping can be copied to destination file.
+     */ 
     void* src = mmap(NULL, info.st_size, PROT_READ, MAP_SHARED, srcFile, 0);
-    void* dest = mmap(NULL, info.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, destFile, 0);
+    if (src == MAP_FAILED)
+    {
+        perror("mmap() error");
+        exit(EXIT_FAILURE);
+    }
 
+    /* Maps address space of destination file.
+     * PROT_WRITE so address space can be written to.
+     */
+    void* dest = mmap(NULL, info.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, destFile, 0);
+    if (dest == MAP_FAILED)
+    {
+        perror("mmap() error");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Copies address space of source file to destination file */
     memcpy(dest, src, info.st_size);
 
+    /* Unmaps source and destination files from memory */
     munmap(src, info.st_size);
     munmap(dest, info.st_size);
 
