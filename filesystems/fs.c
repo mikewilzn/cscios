ed48b66a3100a4b6f76734cec6da4c219923a217
Michael Wilson <mjwilson@millersville.edu>
2022-04-22 16:40:28 -0400

    Implemented fs_create()

--- filesystems/fs.c
+++ filesystems/fs.c
@@ -8,6 +8,7 @@
 #define FS_MAX_FILENAME  16
 #define FS_MAX_FILES     16
 #define FS_BLOCK_SIZE    1024
+#define FS_INODE_SIZE	 56
 
 struct fs_t
 {
@@ -23,145 +24,206 @@ struct inode
 };
 
 // open the file with the above name
-void
+	void
 fs_open (struct fs_t *fs, char diskName[16])
 {
-  // this file will act as the "disk" for your file system
-  if ((fs->fd = open(diskName, O_RDWR)) < 0)
-  {
-	  printf("Error opening filesystem\n");
-	  exit(1);
-  }
-  printf("Disk [ %s ] opened successfully\n", diskName);
+	// this file will act as the "disk" for your file system
+	if ((fs->fd = open(diskName, O_RDWR)) < 0)
+	{
+		printf("Error opening filesystem\n");
+		exit(1);
+	}
+	printf("Disk [ %s ] opened successfully\n", diskName);
 }
 
 // close and clean up all associated things
-void
+	void
 fs_close (struct fs_t *fs)
 {
-  // this file will act as the "disk" for your file system
-  if (close(fs->fd) < 0)
-  {
-	  printf("Error closing filesystem\n");
-	  exit(1);
-  }
-  printf("Filesystem closed successfully\n");
+	// this file will act as the "disk" for your file system
+	if (close(fs->fd) < 0)
+	{
+		printf("Error closing filesystem\n");
+		exit(1);
+	}
+	printf("Filesystem closed successfully\n");
 }
 
 // create a file with this name and this size
-void
+	void
 fs_create (struct fs_t *fs, char name[16], int size)
 {
+	if (size > FS_MAX_FILENAME)
+	{
+		printf("File size must not exceed 8 KB\n");
+	}
 
-  // high level pseudo code for creating a new file
+	char buff[FS_NUM_BLOCKS];
 
-  // Step 1: check to see if we have sufficient free space on disk by
-  // reading in the free block list. To do this:
-  // - move the file pointer to the start of the disk file.
-  // - Read the first 128 bytes (the free/in-use block information)
-  // - Scan the list to make sure you have sufficient free blocks to
-  //   allocate a new file of this size
+	// high level pseudo code for creating a new file
 
-  // Step 2: we look  for a free inode on disk
-  // - Read in a inode
-  // - check the "used" field to see if it is free
-  // - If not, repeat the above two steps until you find a free inode
-  // - Set the "used" field to 1
-  // - Copy the filename to the "name" field
-  // - Copy the file size (in units of blocks) to the "size" field
+	// Step 1: check to see if we have sufficient free space on disk by
+	// reading in the free block list. To do this:
+	// - move the file pointer to the start of the disk file.
+	lseek(fs->fd, 0, SEEK_SET);
+	// - Read the first 128 bytes (the free/in-use block information)
+	read(fs->fd, buff, FS_NUM_BLOCKS);
+	// - Scan the list to make sure you have sufficient free blocks to
+	//   allocate a new file of this size
+	int freeBlocks = 0;
+	for (int i = 0; i < FS_NUM_BLOCKS; ++i)
+	{
+		if (buff[i] == 0)
+			++freeBlocks;
+	}
 
-  // Step 3: Allocate data blocks to the file
-  // - Scan the block list that you read in Step 1 for a free block
-  // - Once you find a free block, mark it as in-use (Set it to 1)
-  // - Set the blockPointer[i] field in the inode to this block number.
-  // - repeat until you allocated "size" blocks
+	if(size > freeBlocks)
+	{
+		printf("Insufficient space for file\n");
+		return;
+	}
 
-  // Step 4: Write out the inode and free block list to disk
-  // - Move the file pointer to the start of the file
-  // - Write out the 128 byte free block list
-  // - Move the file pointer to the position on disk where this inode was stored
-  // - Write out the inode
+	struct inode inode;
+	// Step 2: we look  for a free inode on disk
+	// - Read in a inode
+	lseek(fs->fd, FS_NUM_BLOCKS, SEEK_SET);
+	// - check the "used" field to see if it is free
+	// - If not, repeat the above two steps until you find a free inode
+	// - Set the "used" field to 1
+	// - Copy the filename to the "name" field
+	// - Copy the file size (in units of blocks) to the "size" field
+	int index;
+	for (index = 0; index < FS_MAX_FILES; ++index)
+	{
+		read(fs->fd, &inode, sizeof(inode));
+		
+		if (!strcmp(inode.name, name))
+		{
+			printf("A file with this name already exists.\n");
+			return;
+		}
+		if (inode.used == 0)
+			break;
+
+		if (index >= FS_MAX_FILES)
+		{
+			printf("No free inodes found.\n");
+			return;
+		}
+
+		inode.used = 1;
+		strncpy(inode.name, name, FS_MAX_FILENAME);
+		inode.size = size;
+	}
+
+	// Step 3: Allocate data blocks to the file
+	// - Scan the block list that you read in Step 1 for a free block
+	// - Once you find a free block, mark it as in-use (Set it to 1)
+	// - Set the blockPointer[i] field in the inode to this block number.
+	// - repeat until you allocated "size" blocks
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = 0; j < FS_NUM_BLOCKS; ++j)
+		{
+			if (buff[i] == 0)
+			{
+				buff[i] = 1;
+				inode.blockPointers[i] = j;
+				break;
+			}
+		}
+	}
+
+	// Step 4: Write out the inode and free block list to disk
+	// - Move the file pointer to the start of the file
+	lseek(fs->fd, 0, SEEK_SET);
+	// - Write out the 128 byte free block list
+	write(fs->fd, buff, FS_NUM_BLOCKS);
+	// - Move the file pointer to the position on disk where this inode was stored
+	lseek(fs->fd, FS_NUM_BLOCKS + (index * FS_INODE_SIZE), SEEK_SET);
+	// - Write out the inode
+	write(fs->fd, &inode, sizeof(inode));
 }
 
 // Delete the file with this name
-void
+	void
 fs_delete (struct fs_t *fs, char name[16])
 {
 
-  // Step 1: Locate the inode for this file
-  //   - Move the file pointer to the 1st inode (129th byte)
-  //   - Read in a inode
-  //   - If the iinode is free, repeat above step.
-  //   - If the iinode is in use, check if the "name" field in the
-  //     inode matches the file we want to delete. IF not, read the next
-  //     inode and repeat
+	// Step 1: Locate the inode for this file
+	//   - Move the file pointer to the 1st inode (129th byte)
+	//   - Read in a inode
+	//   - If the iinode is free, repeat above step.
+	//   - If the iinode is in use, check if the "name" field in the
+	//     inode matches the file we want to delete. IF not, read the next
+	//     inode and repeat
 
-  // Step 2: free blocks of the file being deleted
-  // Read in the 128 byte free block list (move file pointer to start
-  //   of the disk and read in 128 bytes)
-  // Free each block listed in the blockPointer fields
+	// Step 2: free blocks of the file being deleted
+	// Read in the 128 byte free block list (move file pointer to start
+	//   of the disk and read in 128 bytes)
+	// Free each block listed in the blockPointer fields
 
-  // Step 3: mark inode as free
-  // Set the "used" field to 0.
+	// Step 3: mark inode as free
+	// Set the "used" field to 0.
 
-  // Step 4: Write out the inode and free block list to disk
-  // Move the file pointer to the start of the file
-  // Write out the 128 byte free block list
-  // Move the file pointer to the position on disk where this inode was stored
-  // Write out the inode
+	// Step 4: Write out the inode and free block list to disk
+	// Move the file pointer to the start of the file
+	// Write out the 128 byte free block list
+	// Move the file pointer to the position on disk where this inode was stored
+	// Write out the inode
 }
 
 // List names of all files on disk
-void
+	void
 fs_ls (struct fs_t *fs)
 {
-  // Step 1: read in each inode and print!
-  // Move file pointer to the position of the 1st inode (129th byte)
-  // for each inode:
-  //   read it in
-  //   if the inode is in-use
-  //     print the "name" and "size" fields from the inode
-  // end for
+	// Step 1: read in each inode and print!
+	// Move file pointer to the position of the 1st inode (129th byte)
+	// for each inode:
+	//   read it in
+	//   if the inode is in-use
+	//     print the "name" and "size" fields from the inode
+	// end for
 }
 
 // read this block from this file
-void
+	void
 fs_read (struct fs_t *fs, char name[16], int blockNum, char buf[1024])
 {
-  // Step 1: locate the inode for this file
-  //   - Move file pointer to the position of the 1st inode (129th byte)
-  //   - Read in a inode
-  //   - If the inode is in use, compare the "name" field with the above file
-  //   - If the file names don't match, repeat
+	// Step 1: locate the inode for this file
+	//   - Move file pointer to the position of the 1st inode (129th byte)
+	//   - Read in a inode
+	//   - If the inode is in use, compare the "name" field with the above file
+	//   - If the file names don't match, repeat
 
-  // Step 2: Read in the specified block
-  // Check that blockNum < inode.size, else flag an error
-  // Get the disk address of the specified block
-  // move the file pointer to the block location
-  // Read in the block! => Read in 1024 bytes from this location into the buffer
-  // "buf"
+	// Step 2: Read in the specified block
+	// Check that blockNum < inode.size, else flag an error
+	// Get the disk address of the specified block
+	// move the file pointer to the block location
+	// Read in the block! => Read in 1024 bytes from this location into the buffer
+	// "buf"
 }
 
 // write this block to this file
-void
+	void
 fs_write (struct fs_t *fs, char name[16], int blockNum, char buf[1024])
 {
 
-  // Step 1: locate the inode for this file
-  // Move file pointer to the position of the 1st inode (129th byte)
-  // Read in a inode
-  // If the inode is in use, compare the "name" field with the above file
-  // If the file names don't match, repeat
+	// Step 1: locate the inode for this file
+	// Move file pointer to the position of the 1st inode (129th byte)
+	// Read in a inode
+	// If the inode is in use, compare the "name" field with the above file
+	// If the file names don't match, repeat
 
-  // Step 2: Write to the specified block
-  // Check that blockNum < inode.size, else flag an error
-  // Get the disk address of the specified block
-  // move the file pointer to the block location
-  // Write the block! => Write 1024 bytes from the buffer "buf" to this location
+	// Step 2: Write to the specified block
+	// Check that blockNum < inode.size, else flag an error
+	// Get the disk address of the specified block
+	// move the file pointer to the block location
+	// Write the block! => Write 1024 bytes from the buffer "buf" to this location
 }
 
 // REPL entry point
-void
+	void
 fs_repl ()
 {
 	/* First line: get name of disk */
@@ -169,5 +231,5 @@ fs_repl ()
 	fgets(diskName, sizeof(diskName), stdin);
 	strtok(diskName, "\n");
 
-	
+
 }
